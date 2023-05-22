a195097e57d5a29b7c9d17f8eb289a250c37e29c
Thomas Boerger <thomas@webhippie.de>
2023-05-22 09:48:09 +0200

    feat: initial commit

---
+++ home/.wezterm.lua
@@ -0,0 +1,173 @@
+local wezterm = require("wezterm")
+
+return {
+    check_for_updates = false,
+    color_scheme = "Solarized Dark (base16)",
+    font_size = 12.0,
+
+    exit_behavior = "Close",
+    scrollback_lines = 100000,
+    enable_scroll_bar = false,
+
+    keys = {
+        {
+            mods = "CTRL|SHIFT",
+            key = "c",
+            action = "Copy"
+        },
+        {
+            mods = "CTRL|SHIFT",
+            key = "v",
+            action = "Paste"
+        },
+
+        {
+            mods = "CTRL",
+            key = "n",
+            action = "SpawnWindow"
+        },
+        {
+            mods = "CTRL|SHIFT",
+            key = "n",
+            action = "SpawnWindow"
+        },
+
+        {
+            mods = "CTRL",
+            key = "t",
+            action = wezterm.action{ SpawnTab="CurrentPaneDomain" }
+        },
+        {
+            mods = "CTRL|SHIFT",
+            key = "t",
+            action = wezterm.action{ SpawnTab="CurrentPaneDomain" }
+        },
+
+        {
+            mods = "CTRL",
+            key = "w",
+            action = wezterm.action{ CloseCurrentTab={ confirm = true } }
+        },
+        {
+            mods = "CTRL",
+            key = "f",
+            action = wezterm.action{ Search={ CaseSensitiveString = "" } }
+        },
+        {
+            mods = "CTRL",
+            key = "k",
+            action = wezterm.action{ ClearScrollback="ScrollbackAndViewport" }
+        },
+        {
+            mods = "CTRL",
+            key = "r",
+            action = "ReloadConfiguration"
+        },
+
+        {
+            mods = "CTRL",
+            key = "1",
+            action = wezterm.action{ ActivateTab=0 }
+        },
+        {
+            mods = "CTRL",
+            key = "2",
+            action = wezterm.action{ ActivateTab=1 }
+        },
+        {
+            mods = "CTRL",
+            key = "3",
+            action = wezterm.action{ ActivateTab=2 }
+        },
+        {
+            mods = "CTRL",
+            key = "4",
+            action = wezterm.action{ ActivateTab=3 }
+        },
+        {
+            mods = "CTRL",
+            key = "5",
+            action = wezterm.action{ ActivateTab=4 }
+        },
+        {
+            mods = "CTRL",
+            key = "6",
+            action = wezterm.action{ ActivateTab=5 }
+        },
+        {
+            mods = "CTRL",
+            key = "7",
+            action = wezterm.action{ ActivateTab=6 }
+        },
+        {
+            mods = "CTRL",
+            key = "8",
+            action = wezterm.action{ ActivateTab=7 }
+        },
+        {
+            mods = "CTRL",
+            key = "9",
+            action = wezterm.action{ ActivateTab=-1 }
+        },
+
+        {
+            mods = "CTRL",
+            key = "PageUp",
+            action = wezterm.action{ ActivateTabRelative=-1 }
+        },
+        {
+            mods = "CTRL",
+            key = "PageDown",
+            action = wezterm.action{ ActivateTabRelative=1 }
+        },
+        {
+            mods = "CTRL",
+            key = "Tab",
+            action = wezterm.action{ ActivateTabRelative=1 }
+        },
+
+        {
+            mods = "CTRL|SHIFT",
+            key = "PageUp",
+            action = wezterm.action{ MoveTabRelative=-1 }
+        },
+        {
+            mods = "CTRL|SHIFT",
+            key = "PageDown",
+            action = wezterm.action{ MoveTabRelative=1 }
+        },
+
+        {
+            mods = "SHIFT",
+            key = "PageUp",
+            action = wezterm.action{ ScrollByPage=-1 }
+        },
+        {
+            mods = "SHIFT",
+            key = "PageDown",
+            action = wezterm.action{ ScrollByPage=1 }
+        },
+
+        {
+            mods = "ALT",
+            key = "Enter",
+            action = "ToggleFullScreen"
+        },
+
+        {
+            mods = "CTRL",
+            key = "-",
+            action = "DecreaseFontSize"
+        },
+        {
+            mods = "CTRL",
+            key = "+",
+            action = "IncreaseFontSize"
+        },
+        {
+            mods = "CTRL",
+            key = "0",
+            action = "ResetFontSize"
+        }
+    }
+}
